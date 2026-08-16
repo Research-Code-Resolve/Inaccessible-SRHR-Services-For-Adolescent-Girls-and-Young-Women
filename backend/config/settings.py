c23cf22954d9b3c3f198d9fe4a7a8c7efe93a165
Joy Bett <cjoybett@gmail.com>
2026-08-16 04:09:42 +0300

    Add new apps to Django settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'accounts',
     'providers',
+    'education',
+    'appointments',
+    'tracker',
+    'emergency',
+    'accounts',
     'triage',
-    'support',
+
 
 ]
 
